3ed2f34d8857e494f793903e90e60132aad779e8
awalehdek <awalehdek@gmail.com>
2024-10-23 18:38:26 -0700

    sigmoid & load extra datasets

--- 1_1_Planar data classificaion with one hidden layer/planar_utils.py
+++ 1_1_Planar data classificaion with one hidden layer/planar_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import datasets
 
 def load_planar_dataset():
     np.random.seed(1)
@@ -34,3 +35,52 @@ def plot_decision_boundary(model, X, y):
     plt.xlabel('x1')
     plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
     plt.show()
+
+
+
+def sigmoid(z):
+    """
+    Computes the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Returns:
+    s -- sigmoid(z)
+    """
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+
+from sklearn import datasets
+
+
+def load_extra_datasets():
+    """
+    Loads additional datasets for testing classification algorithms.
+
+    Returns:
+    datasets -- a dictionary containing various datasets ('moons', 'circles', 'blobs', and 'gaussian_quantiles')
+    """
+    np.random.seed(0)
+    N = 200  # number of points per class
+
+    # Moons dataset
+    noisy_moons = datasets.make_moons(n_samples=N, noise=0.2)
+
+    # Circles dataset
+    noisy_circles = datasets.make_circles(n_samples=N, factor=0.5, noise=0.3)
+
+    # Blobs dataset
+    blobs = datasets.make_blobs(n_samples=N, random_state=5, n_features=2, centers=2, cluster_std=1.0)
+
+    # Gaussian Quantiles
+    gaussian_quantiles = datasets.make_gaussian_quantiles(mean=None, cov=0.9, n_samples=N, n_features=2, n_classes=2)
+
+    # Dictionary of datasets
+    datasets = {"noisy_moons": noisy_moons,
+                "noisy_circles": noisy_circles,
+                "blobs": blobs,
+                "gaussian_quantiles": gaussian_quantiles}
+
+    return datasets
